d808049eaa4c132d909323fbbfeecd430ce429be
Brunob0797 <31026046+Brunob0797@users.noreply.github.com>
2017-08-15 11:05:31 -0300

    Add files via upload

---
+++ comandos/frente.php
@@ -0,0 +1,18 @@
+﻿<?php
+	/*
+		Motor Direito;
+		Identificação de pinos BCM;
+		Pinos 23 e 24;
+		Ligado = 1; Desligado = 0;
+	*/
+	system("gpio -g write 17 1");
+	system("gpio -g write 27 0");
+	/*
+		Motor Esquerdo;
+		Identificação de pinos BCM;
+		Pinos 23 e 24;
+		Ligado = 1; Desligado = 0;
+	*/
+	system("gpio -g write 23 1");
+	system("gpio -g write 24 0");
+?>
---
+++ comandos/tras.php
@@ -0,0 +1,18 @@
+﻿<?php
+	/*
+		Motor Direito;
+		Identificação de pinos BCM;
+		Pinos 23 e 24;
+		Ligado = 1; Desligado = 0;
+	*/
+	system("gpio -g write 17 0");
+	system("gpio -g write 27 1");
+	/*
+		Motor Esquerdo;
+		Identificação de pinos BCM;
+		Pinos 23 e 24;
+		Ligado = 1; Desligado = 0;
+	*/
+	system("gpio -g write 23 0");
+	system("gpio -g write 24 1");
+?>
